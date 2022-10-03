762b24d9308bd92efdd545a9c80d5c361bf8418e
Tony <gth1123@naver.com>
2022-10-03 23:41:49 +0900

    useSelector 쓸때마다 타입은 지정해줘야 될 듯

--- rnTsSample/src/states/redux/store/index.ts
+++ rnTsSample/src/states/redux/store/index.ts
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from '../slice';
+import rootReducer, { RootState } from '../slice';
 
 const store = configureStore({
   reducer: rootReducer,
 });
+
+declare module 'react-redux' {
+  interface DefaultRootState extends RootState {}
+}
 
 export default store;
