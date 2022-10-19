732c5fc7eab9d49f8a7c0962ab27fc3628e9a5fc
Tony <gth1123@naver.com>
2022-10-19 22:29:43 +0900

    15.1.2 리액트 네비게이션 적용

---
+++ ArticlesApp/src/screens/ArticlesScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function ArticlesScreen() {
+  return (
+    <View>
+      <Text>ArticlesScreen</Text>
+    </View>
+  );
+}
+
+export default ArticlesScreen;
