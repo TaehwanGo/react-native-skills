4e35239e615120a57bb12c5c01d79c03f1678d1c
Tony <gth1123@naver.com>
2022-10-28 23:14:32 +0900

    15.9.5 ~ 15.9.7 게시글 등록 후 업데이트

--- ArticlesApp/src/components/screens/WriteScreen.tsx
+++ ArticlesApp/src/components/screens/WriteScreen.tsx
@@ -10,16 +10,35 @@ import {
 import {SafeAreaView, useSafeAreaInsets} from 'react-native-safe-area-context';
 import {RootStackNavigationProp} from '../../types/screens';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import {useMutation, useQueryClient} from 'react-query';
+import {writeArticle} from '../../api/articles';
+import {Article} from '../../types/api';
 
 function WriteScreen() {
   const {top} = useSafeAreaInsets();
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
+  const queryClient = useQueryClient();
+  const {mutate: write} = useMutation(writeArticle, {
+    onSuccess: article => {
+      // queryClient.invalidateQueries('articles'); // 새로운 글을 작성하면 articles 쿼리를 다시 요청하도록 설정
+
+      // 캐시 데이터에 새로운 글을 추가
+      // const articles = queryClient.getQueryData<Article[]>('articles') ?? []; // articles 쿼리의 (캐싱된)데이터를 가져옴
+      // queryClient.setQueryData('articles', articles.concat(article)); // 새로운 글을 articles 쿼리의 데이터에 추가
+
+      // 캐시 데이터에 새로운 글 추가 축약버전
+      queryClient.setQueryData<Article[]>('articles', articles => {
+        return (articles ?? []).concat(article);
+      });
+      navigation.goBack();
+    },
+  });
 
   const navigation = useNavigation<RootStackNavigationProp>();
   const onSubmit = useCallback(() => {
-    // TODO : 구현 예정
-  }, []);
+    write({title, body});
+  }, [title, body, write]);
 
   useEffect(() => {
     navigation.setOptions({
