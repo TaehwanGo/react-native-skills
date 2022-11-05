182b6dc891ff0e58cf796fa35c6db025a0ba87ce
Tony <gth1123@naver.com>
2022-11-05 22:04:38 +0900

    15.12.3 댓글 삭제 기능 구현

--- ArticlesApp/src/components/CommentItem.tsx
+++ ArticlesApp/src/components/CommentItem.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
 
 export interface CommentItemProps {
   id: number;
   message: string;
   username: string;
   publishedAt: string;
+  isMyComment: boolean;
+  onRemove(id: number): void;
+  onModify(id: number): void;
 }
-function CommentItem({message, username, publishedAt}: CommentItemProps) {
+function CommentItem({
+  id,
+  message,
+  username,
+  publishedAt,
+  isMyComment,
+  onRemove,
+  onModify,
+}: CommentItemProps) {
   const formattedDate = new Date(publishedAt).toLocaleDateString('ko-KR', {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
   });
+  const handleRemove = () => {
+    onRemove(id);
+  };
+  const handleModify = () => {
+    onModify(id);
+  };
   return (
     <View style={styles.block}>
       <View style={styles.head}>
@@ -20,6 +37,23 @@ function CommentItem({message, username, publishedAt}: CommentItemProps) {
         <Text style={styles.date}>{formattedDate}</Text>
       </View>
       <Text style={styles.message}>{message}</Text>
+      {isMyComment && (
+        <View style={styles.actionButtons}>
+          <Pressable
+            style={({pressed}) => pressed && styles.pressed}
+            hitSlop={8}
+            onPress={handleModify}>
+            <Text style={styles.buttonText}>수정</Text>
+          </Pressable>
+          <View style={styles.separator} />
+          <Pressable
+            style={({pressed}) => pressed && styles.pressed}
+            hitSlop={8}
+            onPress={handleRemove}>
+            <Text style={styles.buttonText}>삭제</Text>
+          </Pressable>
+        </View>
+      )}
     </View>
   );
 }
@@ -43,6 +77,21 @@ const styles = StyleSheet.create({
   },
   message: {
     marginTop: 4,
+  },
+  actionButtons: {
+    marginTop: 24,
+    justifyContent: 'flex-end',
+    flexDirection: 'row',
+  },
+  separator: {
+    width: 8,
+  },
+  buttonText: {
+    fontSize: 12,
+    color: '#546e7a',
+  },
+  pressed: {
+    opacity: 0.75,
   },
 });
 
--- ArticlesApp/src/components/screens/ArticleScreen.tsx
+++ ArticlesApp/src/components/screens/ArticleScreen.tsx
@@ -1,13 +1,15 @@
 import {RouteProp, useRoute} from '@react-navigation/native';
-import React from 'react';
+import React, {useState} from 'react';
 import {ActivityIndicator, FlatList, StyleSheet} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
-import {useQuery} from 'react-query';
+import {useMutation, useQuery, useQueryClient} from 'react-query';
 import {getArticle} from '../../api/articles';
-import {getComments} from '../../api/comments';
+import {deleteComment, getComments} from '../../api/comments';
 import {useUserState} from '../../states/context/UserContext';
+import {Comment} from '../../types/api';
 import {RootStackParamList} from '../../types/screens';
 import ArticleView from '../ArticleView';
+import AskDialog from '../AskDialog';
 import CommentInput from '../CommentInput';
 import CommentItem from '../CommentItem';
 
@@ -23,6 +25,44 @@ function ArticleScreen() {
 
   const {bottom} = useSafeAreaInsets(); // 하단 필수 여백 - iOS 홈버튼 없는 것 대응
 
+  const [selectedCommentId, setSelectedCommentId] = useState<number | null>(
+    null,
+  );
+  const [askRemoveComment, setAskRemoveComment] = useState<boolean>(false);
+
+  const queryClient = useQueryClient();
+  const {mutate: removeComment} = useMutation(deleteComment, {
+    onSuccess: () => {
+      queryClient.setQueryData<Comment[]>(['comments', id], comments =>
+        comments
+          ? comments.filter(comment => comment.id !== selectedCommentId)
+          : [],
+      );
+    },
+  });
+
+  const onRemoveComment = (commentId: number) => {
+    setSelectedCommentId(commentId);
+    setAskRemoveComment(true);
+  };
+
+  const onConfirmRemoveComment = () => {
+    setAskRemoveComment(false);
+    removeComment({
+      id: selectedCommentId!,
+      articleId: id,
+    });
+  };
+
+  const onCancelRemoveComment = () => {
+    setAskRemoveComment(false);
+  };
+
+  const onModifyComment = (commentId: number) => {
+    console.log('modify comment', commentId);
+    setAskRemoveComment(false);
+  };
+
   if (!articleQuery.data || !commentsQuery.data) {
     return (
       <ActivityIndicator size="large" style={styles.spinner} color="black" />
@@ -33,33 +73,50 @@ function ArticleScreen() {
   const isMyArticle = currentUser?.id === user.id;
 
   return (
-    <FlatList
-      style={styles.flatList}
-      contentContainerStyle={[styles.flatListContent, {paddingBottom: bottom}]}
-      data={commentsQuery.data}
-      keyExtractor={item => item.id.toString()}
-      renderItem={({item}) => (
-        <CommentItem
-          id={item.id}
-          message={item.message}
-          publishedAt={item.published_at}
-          username={item.user.username}
-        />
-      )}
-      ListHeaderComponent={
-        <>
-          <ArticleView
-            title={title}
-            body={body}
-            publishedAt={published_at}
-            username={user.username}
-            id={id}
-            isMyArticle={isMyArticle}
+    <>
+      <FlatList
+        style={styles.flatList}
+        contentContainerStyle={[
+          styles.flatListContent,
+          {paddingBottom: bottom},
+        ]}
+        data={commentsQuery.data}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({item}) => (
+          <CommentItem
+            id={item.id}
+            message={item.message}
+            publishedAt={item.published_at}
+            username={item.user.username}
+            isMyComment={currentUser?.id === item.user.id}
+            onRemove={onRemoveComment}
+            onModify={onModifyComment}
           />
-          <CommentInput articleId={id} />
-        </>
-      }
-    />
+        )}
+        ListHeaderComponent={
+          <>
+            <ArticleView
+              title={title}
+              body={body}
+              publishedAt={published_at}
+              username={user.username}
+              id={id}
+              isMyArticle={isMyArticle}
+            />
+            <CommentInput articleId={id} />
+          </>
+        }
+      />
+      <AskDialog
+        visible={askRemoveComment}
+        title="댓글 삭제"
+        message="댓글을 삭제하시겠습니까?"
+        isDestructive
+        confirmText="삭제"
+        onConfirm={onConfirmRemoveComment}
+        onClose={onCancelRemoveComment}
+      />
+    </>
   );
 }
 
